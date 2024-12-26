eee53c5ed979e549243baf01f544234a4f95eb3f
Oscar Marin <oscar@marin.cr>
2024-12-26 14:55:01 -0600

    improvements

--- src/Luval.GenAIBotMate.Components/GenAIBotMateControl.razor.cs
+++ src/Luval.GenAIBotMate.Components/GenAIBotMateControl.razor.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.FluentUI.AspNetCore.Components;
+﻿using Microsoft.FluentUI.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Luval.GenAIBotMate.Components
 {
-    public partial class GenAIBot : FluentComponentBase  
+    public partial class GenAIBotMateControl : FluentComponentBase
     {
     }
 }
