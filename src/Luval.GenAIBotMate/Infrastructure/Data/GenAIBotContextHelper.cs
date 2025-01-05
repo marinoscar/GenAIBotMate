a293043d67cdacc1f0faeab206b7211c11db2833
Oscar Marin <oscar@marin.cr>
2025-01-05 11:32:10 -0600

    Update GenAIBotContextHelper.cs

--- src/Luval.GenAIBotMate/Infrastructure/Data/GenAIBotContextHelper.cs
+++ src/Luval.GenAIBotMate/Infrastructure/Data/GenAIBotContextHelper.cs
@@ -17,28 +17,31 @@ namespace Luval.GenAIBotMate.Infrastructure.Data
     public class GenAIBotContextHelper
     {
         private readonly ILogger<GenAIBotContextHelper> _logger;
+        private readonly IChatDbContext _context;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="GenAIBotContextHelper"/> class with the specified logger.
         /// </summary>
+        /// <param name="context">The database context to use for initialization.</param>
         /// <param name="logger">The logger to use for logging operations.</param>
         /// <exception cref="ArgumentNullException">Thrown when the logger is null.</exception>
-        public GenAIBotContextHelper(ILogger<GenAIBotContextHelper> logger)
+        public GenAIBotContextHelper(IChatDbContext context, ILogger<GenAIBotContextHelper> logger)
         {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="GenAIBotContextHelper"/> class with a default logger.
         /// </summary>
-        public GenAIBotContextHelper() : this(new ColorConsoleLogger<GenAIBotContextHelper>())
+        /// <param name="context">The database context to use for initialization.</param>
+        public GenAIBotContextHelper(IChatDbContext context) : this(context, new ColorConsoleLogger<GenAIBotContextHelper>())
         {
         }
 
         /// <summary>
         /// Initializes the database context by ensuring the database is created, applying any pending migrations, and seeding initial data.
         /// </summary>
-        /// <param name="context">The database context to initialize.</param>
         /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
         /// <remarks>
         /// This method performs the following actions:
@@ -46,21 +49,24 @@ namespace Luval.GenAIBotMate.Infrastructure.Data
         /// 2. If the database can be connected to, it checks for any pending migrations and applies them if necessary.
         /// 3. Seeds the database with initial data if the GenAIBots table is empty.
         /// </remarks>
-        public async Task InitializeAsync(IChatDbContext context, CancellationToken cancellationToken = default)
+        public async Task InitializeAsync(CancellationToken cancellationToken = default)
         {
             _logger.LogInformation("Initializing database context...");
             // Make sure the DB is created
-            if (!await context.Database.CanConnectAsync())
+            if (!await _context.Database.CanConnectAsync())
             {
                 _logger.LogInformation("Database does not exist. Running first migration...");
-                var createScript = context.Database.GenerateCreateScript();
-                await context.Database.ExecuteSqlRawAsync(createScript, cancellationToken);
+                await CreateDatabaseAsync(cancellationToken);
             }
-            if (!(await context.GenAIBots.AnyAsync(cancellationToken)))
+            if (!CheckForTables())
+            {
+                await CreateDatabaseAsync(cancellationToken);
+            }
+            if (!(await _context.GenAIBots.AnyAsync(cancellationToken)))
             {
                 // Add some data
                 _logger.LogInformation("Seeding initial data...");
-                context.GenAIBots.Add(new GenAIBot
+                _context.GenAIBots.Add(new GenAIBot
                 {
                     Name = "Gen AI Bot",
                     UtcCreatedOn = DateTime.UtcNow,
@@ -69,9 +75,29 @@ namespace Luval.GenAIBotMate.Infrastructure.Data
                     UpdatedBy = "System",
                     Version = 1
                 });
-                await context.SaveChangesAsync(cancellationToken);
+                await _context.SaveChangesAsync(cancellationToken);
                 _logger.LogInformation("Initial data seeded.");
             }
         }
-    }
+
+        private async Task CreateDatabaseAsync(CancellationToken cancellationToken = default)
+        {
+            var createScript = _context.Database.GenerateCreateScript();
+            await _context.Database.ExecuteSqlRawAsync(createScript, cancellationToken);
+        }
+
+        private bool CheckForTables(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                // Check for the GenAIBots table
+                _context.GenAIBots.Select(c => c.Id).Take(1).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Schema not created.");
+                return false;
+            }
+            return true;
+        }
 }
