decbe9a47f3aadfc3337de949f850933f2155e33
Oscar Marin <oscar@marin.cr>
2025-01-17 21:49:53 -0600

    Event handling
    changed how the events are handled to avoid issues with the scope of the DbContext

--- src/Luval.GenAIBotMate.Components/Infrastructure/Data/HistoryDto.cs
+++ src/Luval.GenAIBotMate.Components/Infrastructure/Data/HistoryDto.cs
@@ -7,13 +7,24 @@ using System.Threading.Tasks;
 
 namespace Luval.GenAIBotMate.Components.Infrastructure.Data
 {
+    /// <summary>
+    /// Data transfer object for managing chat history.
+    /// </summary>
     public class HistoryDto
     {
+        /// <summary>
+        /// Collection of chat sessions.
+        /// </summary>
         public IQueryable<ChatSession> Sessions { get; set; } = default!;
 
-        public Func<ChatSession, Task> NavigateFunction { get; set; } = default!;
-
-        public Func<ChatSession, Task> DeleteFunction { get; set; } = default!;
+        /// <summary>
+        /// Indicates the ID of the session selected by the user
+        /// </summary>
+        public ulong? SessionId { get; set; } = default!;
+        /// <summary>
+        /// Indicates if the user has selected to delete a session
+        /// </summary>
+        public bool? IsDelete { get; set; } = default!;
 
     }
 }
